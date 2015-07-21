f064b77376bbd40ec5f71af7620fc840538c8905
Andreas Falk <mail@andreasfalk.se>
2015-07-21 16:13:14 +0200

    style: remove star import from LabelsTest

--- core/src/test/java/se/tre/freki/labels/LabelsTest.java
+++ core/src/test/java/se/tre/freki/labels/LabelsTest.java
@@ -1,6 +1,7 @@
 package se.tre.freki.labels;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
