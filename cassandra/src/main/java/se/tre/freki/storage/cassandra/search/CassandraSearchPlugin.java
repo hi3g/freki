e1033c77460f292cee150dfd5c76170aabd3786a
Johannes <johannes.moberg@tre.se>
2015-07-22 17:13:59 +0200

    feat: Searching for ngrams returns a Iterable of LabelMetas

--- cassandra/src/main/java/se/tre/freki/storage/cassandra/search/CassandraSearchPlugin.java
+++ cassandra/src/main/java/se/tre/freki/storage/cassandra/search/CassandraSearchPlugin.java
@@ -5,7 +5,11 @@ import static com.datastax.driver.core.querybuilder.QueryBuilder.eq;
 import static com.datastax.driver.core.querybuilder.QueryBuilder.insertInto;
 import static com.datastax.driver.core.querybuilder.QueryBuilder.select;
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.util.concurrent.Futures.addCallback;
 import static com.google.common.util.concurrent.Futures.allAsList;
+import static com.google.common.util.concurrent.Futures.immediateFuture;
+import static com.google.common.util.concurrent.Futures.successfulAsList;
+import static com.google.common.util.concurrent.Futures.transform;
 
 import se.tre.freki.BuildData;
 import se.tre.freki.labels.LabelId;
@@ -21,16 +25,18 @@ import se.tre.freki.storage.cassandra.functions.ToVoidFunction;
 import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.ResultSetFuture;
+import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
-import com.google.common.collect.ImmutableSet;
+import com.google.common.base.Function;
+import com.google.common.util.concurrent.AsyncFunction;
 import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import javax.annotation.Nonnull;
 
@@ -42,9 +48,9 @@ public class CassandraSearchPlugin extends SearchPlugin {
   private final PreparedStatement selectNGramStatment;
 
   /**
-   * Creates a new instance of a CassandraSearchPlugin.
+   * Creates a new instance of a {@link CassandraSearchPlugin}.
    *
-   * @param cassandraStore an instance of a CassandraStore
+   * @param cassandraStore an instance of a {@link CassandraStore}.
    */
   public CassandraSearchPlugin(final CassandraStore cassandraStore) {
     this.session = checkNotNull(cassandraStore.getSession());
@@ -80,40 +86,131 @@ public class CassandraSearchPlugin extends SearchPlugin {
 
     final NGramGenerator ngram = new NGramGenerator(meta.name());
 
-    ListenableFuture<Void> result = writeNGramsIndex(ngram, (CassandraLabelId) meta.identifier(),
-        meta.type());
-
-    return result;
+    return writeNGramsIndex(ngram, (CassandraLabelId) meta.identifier(), meta.type());
   }
 
   @Nonnull
   @Override
   public ListenableFuture<Void> deleteLabelMeta(final LabelId id,
                                                 final LabelType type) {
-    return Futures.immediateFuture(null);
+    return immediateFuture(null);
   }
 
   @Nonnull
   @Override
   public ListenableFuture<Void> indexAnnotation(final Annotation note) {
-    return Futures.immediateFuture(null);
+    return immediateFuture(null);
   }
 
   @Nonnull
   @Override
   public ListenableFuture<Void> deleteAnnotation(final Annotation note) {
-    return Futures.immediateFuture(null);
+    return immediateFuture(null);
   }
 
   @Nonnull
   @Override
   public ListenableFuture<Iterable<LabelMeta>> findLabels(final String query) {
-    return Futures.<Iterable<LabelMeta>>immediateFuture(ImmutableSet.<LabelMeta>of());
+    final NGramGenerator ngrams = new NGramGenerator(query);
+    final List<ResultSetFuture> resultSets = fetchLabelIds(ngrams);
+
+    return transform(
+        successfulAsList(resultSets),
+        new AsyncFunction<List<ResultSet>, Iterable<LabelMeta>>() {
+          @Override
+          public ListenableFuture<Iterable<LabelMeta>> apply(final List<ResultSet> resultSets) {
+            return transform(fetchLabelMetas(resultSets),
+                new Function<Iterable<LabelMeta>, Iterable<LabelMeta>>() {
+                  @Override
+                  public Iterable<LabelMeta> apply(final Iterable<LabelMeta> input) {
+                    return input;
+                  }
+                });
+          }
+        });
   }
 
   /**
-   * Takes an {@link NGramGenerator} generated from a labelIds name and creates an index for this
-   * labelId in the database.
+   * Takes a {@link List} of {@link ResultSetFuture}s and for each label id in the {@link
+   * ResultSet}s gets the corresponding {@link LabelMeta}.
+   *
+   * @param resultSets A List of ResultSets
+   * @return A {@link ListenableFuture} with a {@link List} of {@link LabelMeta}s.
+   */
+  protected ListenableFuture<? extends Iterable<LabelMeta>> fetchLabelMetas(
+      final List<ResultSet> resultSets) {
+
+    final List<ListenableFuture<LabelMeta>> metas = new ArrayList<>();
+
+    for (final ResultSet resultSet : resultSets) {
+      for (final Row row : resultSet) {
+        final CassandraLabelId id = CassandraLabelId.fromLong(row.getLong("label_id"));
+        metas.add(cassandraStore.getMeta(id, id.type()));
+      }
+    }
+
+    return allAsList(metas);
+  }
+
+  /**
+   * Takes a {@link Iterator} of {@link String}s and queries cassandra for label ids matching each
+   * n-gram in the {@link Iterator} of {@link String}s supplied.
+   *
+   * @param ngrams A Iterator of Strings that we want to find the corresponding label ids for.
+   * @return A {@link ResultSetFuture} with the corresponding query {@link ResultSet}.
+   */
+  protected List<ResultSetFuture> fetchLabelIds(final Iterator<String> ngrams) {
+    final List<ResultSetFuture> resultSets = new ArrayList<>();
+
+    while (ngrams.hasNext()) {
+      final String ngram = ngrams.next();
+      if (ngram.isEmpty()) {
+        return resultSets;
+      }
+      final ResultSetFuture metricResult = labelIdQuery(ngram, LabelType.METRIC);
+      final ResultSetFuture tagkResult = labelIdQuery(ngram, LabelType.TAGK);
+      final ResultSetFuture tagvResult = labelIdQuery(ngram, LabelType.TAGV);
+
+      resultSets.add(metricResult);
+      resultSets.add(tagkResult);
+      resultSets.add(tagvResult);
+    }
+    return resultSets;
+  }
+
+  /**
+   * Takes a n-gram and a {@link LabelType} and queries cassandra for the {@link CassandraLabelId}s
+   * that matches the n-gram and type and returns this a {@link ResultSetFuture}.
+   *
+   * @param ngram an n-gram used to lookup {@link CassandraLabelId}.
+   * @param type The {@link LabelType} of the label we look for.
+   * @return A {@link ResultSetFuture} containing a {@link ResultSet} for the queried n-gram and
+   * {@link LabelType}.
+   */
+  protected ResultSetFuture labelIdQuery(final String ngram, final LabelType type) {
+
+    final ResultSetFuture result = session.executeAsync(selectNGramStatment.bind()
+        .setString(0, ngram)
+        .setString(1, type.toValue()));
+
+    addCallback(result, new FutureCallback<ResultSet>() {
+      @Override
+      public void onSuccess(final ResultSet result) {
+        //cassandra call succeeded
+      }
+
+      @Override
+      public void onFailure(final Throwable throwable) {
+        LOG.error("Search for n-gram {} of type {} failed", ngram, type, throwable);
+      }
+    });
+
+    return result;
+  }
+
+  /**
+   * Takes an {@link NGramGenerator} generated from a label IDs name and creates an index for this
+   * label ID in the database.
    *
    * @param ngrams An iterator over a strings 3-grams.
    * @param labelId A labels unique ID.
@@ -123,15 +220,15 @@ public class CassandraSearchPlugin extends SearchPlugin {
   private ListenableFuture<Void> writeNGramsIndex(final NGramGenerator ngrams,
                                                   final CassandraLabelId labelId,
                                                   final LabelType type) {
-    List<ResultSetFuture> list = new ArrayList<>();
+    final List<ResultSetFuture> list = new ArrayList<>();
 
     while (ngrams.hasNext()) {
       final String nGram = ngrams.next();
-      ResultSetFuture result = session.executeAsync(createNGramStatement.bind()
+      final ResultSetFuture result = session.executeAsync(createNGramStatement.bind()
           .setString(0, nGram)
           .setString(1, type.toValue())
           .setLong(2, CassandraLabelId.toLong(labelId)));
-      Futures.addCallback(result, new FutureCallback<ResultSet>() {
+      addCallback(result, new FutureCallback<ResultSet>() {
 
         @Override
         public void onSuccess(final ResultSet result) {
@@ -140,11 +237,12 @@ public class CassandraSearchPlugin extends SearchPlugin {
 
         @Override
         public void onFailure(final Throwable throwable) {
-          LOG.error("Write of ngram {} with id {} of type {}", nGram, labelId, type );
+          LOG.error("Write of n-gram {} with id {} of type {} failed", nGram, labelId, type,
+              throwable);
         }
       });
       list.add(result);
     }
-    return Futures.transform(allAsList(list), new ToVoidFunction());
+    return transform(allAsList(list), new ToVoidFunction());
   }
 }
--- cassandra/src/test/java/se/tre/freki/storage/cassandra/search/CassandraSearchPluginTest.java
+++ cassandra/src/test/java/se/tre/freki/storage/cassandra/search/CassandraSearchPluginTest.java
@@ -1,5 +1,8 @@
 package se.tre.freki.storage.cassandra.search;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import se.tre.freki.labels.LabelType;
 import se.tre.freki.meta.LabelMeta;
 import se.tre.freki.storage.cassandra.CassandraLabelId;
@@ -10,10 +13,16 @@ import se.tre.freki.storage.cassandra.CassandraTestHelpers;
 import se.tre.freki.storage.cassandra.DaggerCassandraTestComponent;
 
 import com.codahale.metrics.MetricRegistry;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.ResultSetFuture;
+import com.datastax.driver.core.Row;
+import com.google.common.collect.ImmutableSet;
 import com.typesafe.config.Config;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
 
 public class CassandraSearchPluginTest {
   private CassandraStoreDescriptor storeDescriptor;
@@ -43,12 +52,34 @@ public class CassandraSearchPluginTest {
   public void testNGram() throws Exception {
     LabelMeta meta = LabelMeta.create(CassandraLabelId.fromLong(23L), LabelType.METRIC, "jungle",
         "and its description", 0L);
+    LabelMeta meta2 = LabelMeta.create(CassandraLabelId.fromLong(22L), LabelType.METRIC, "junglar",
+        "and its description", 0L);
 
+    searchPlugin.indexLabelMeta(meta2);
     searchPlugin.indexLabelMeta(meta);
   }
 
   @Test
-  public void testSearchLabel() throws Exception {
-    searchPlugin.findLabels("jungle");
+  public void testFetchLabelIdsForEmptyString() throws Exception {
+    List<ResultSetFuture> result = searchPlugin.fetchLabelIds(ImmutableSet.<String>of().iterator());
+    assertTrue(result.isEmpty());
+  }
+
+  @Test
+  public void testLabelIdsForExistingNgram() throws Exception {
+    LabelMeta meta = LabelMeta.create(CassandraLabelId.fromLong(23L), LabelType.METRIC, "jungle",
+        "and its description", 0L);
+    searchPlugin.indexLabelMeta(meta).get();
+
+    final NGramGenerator ngram = new NGramGenerator("jungle");
+    final List<ResultSetFuture> result = searchPlugin.fetchLabelIds(ngram);
+
+    for (final ResultSetFuture resultSetFuture : result) {
+      ResultSet rs = resultSetFuture.get();
+      for (final Row r : rs) {
+        assertEquals(23L, r.getLong("label_id"));
+        assertEquals(LabelType.METRIC.toValue(), r.getString(1));
+      }
+    }
   }
 }
