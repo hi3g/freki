855cd42623675fce1daf02c4996618954294e43f
Andreas Falk <mail@andreasfalk.se>
2015-07-29 11:20:20 +0200

    feat: write the full metric and tags to the inverted time series index

--- cassandra/src/main/java/se/tre/freki/storage/cassandra/IndexStrategy.java
+++ cassandra/src/main/java/se/tre/freki/storage/cassandra/IndexStrategy.java
@@ -10,10 +10,12 @@ import se.tre.freki.labels.LabelType;
 
 import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.Session;
+import com.google.common.collect.ImmutableMap;
 
 import java.nio.ByteBuffer;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * An indexing strategy defines the behavior for how time series IDs should be indexed. The
@@ -58,17 +60,24 @@ interface IndexStrategy {
           insertInto(Tables.TS_INVERTED_INDEX)
               .value("label_id", bindMarker())
               .value("type", bindMarker())
-              .value("timeseries_id", bindMarker()));
+              .value("timeseries_id", bindMarker())
+              .value("metric", bindMarker())
+              .value("tags", bindMarker()));
     }
 
     @Override
     public void indexTimeseriesId(final LabelId metric,
                                   final List<LabelId> tags,
                                   final ByteBuffer timeSeriresId) {
+      final long longMetric = toLong(metric);
+      final Map<Long, Long> longTags = toMap(tags);
+
       session.executeAsync(insertTagsStatement.bind()
-          .setLong(0, toLong(metric))
+          .setLong(0, longMetric)
           .setString(1, LabelType.METRIC.toValue())
-          .setBytesUnsafe(2, timeSeriresId));
+          .setBytesUnsafe(2, timeSeriresId)
+          .setLong(3, longMetric)
+          .setMap(4, longTags));
 
       final Iterator<LabelId> tagIterator = tags.iterator();
 
@@ -76,13 +85,30 @@ interface IndexStrategy {
         session.executeAsync(insertTagsStatement.bind()
             .setLong(0, toLong(tagIterator.next()))
             .setString(1, LabelType.TAGK.toValue())
-            .setBytesUnsafe(2, timeSeriresId));
+            .setBytesUnsafe(2, timeSeriresId)
+            .setLong(3, longMetric)
+            .setMap(4, longTags));
 
         session.executeAsync(insertTagsStatement.bind()
             .setLong(0, toLong(tagIterator.next()))
             .setString(1, LabelType.TAGV.toValue())
-            .setBytesUnsafe(2, timeSeriresId));
+            .setBytesUnsafe(2, timeSeriresId)
+            .setLong(3, longMetric)
+            .setMap(4, longTags));
       }
+    }
+
+    private Map<Long, Long> toMap(List<LabelId> tags) {
+      final Iterator<LabelId> iterator = tags.iterator();
+      final ImmutableMap.Builder<Long, Long> mapTags = ImmutableMap.builder();
+
+      while (iterator.hasNext()) {
+        final long tagk = toLong(iterator.next());
+        final long tagv = toLong(iterator.next());
+        mapTags.put(tagk, tagv);
+      }
+
+      return mapTags.build();
     }
   }
 }
