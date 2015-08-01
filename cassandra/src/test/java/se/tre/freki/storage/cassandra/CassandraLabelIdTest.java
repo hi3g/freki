95e5e22ccb39cf8eb5331c9f9bf7d850a5ef81ab
Andreas Falk <mail@andreasfalk.se>
2015-08-01 17:48:50 +0200

    test: test CassandraLabelId#equals

--- cassandra/src/test/java/se/tre/freki/storage/cassandra/CassandraLabelIdTest.java
+++ cassandra/src/test/java/se/tre/freki/storage/cassandra/CassandraLabelIdTest.java
@@ -1,10 +1,14 @@
 package se.tre.freki.storage.cassandra;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static se.tre.freki.labels.LabelType.METRIC;
 import static se.tre.freki.labels.LabelType.TAGK;
 import static se.tre.freki.labels.LabelType.TAGV;
 import static se.tre.freki.storage.cassandra.CassandraLabelId.makeIdTypeSpecific;
+
+import se.tre.freki.labels.LabelId;
 
 import com.google.common.hash.Hashing;
 import org.junit.Test;
@@ -16,6 +20,20 @@ public class CassandraLabelIdTest {
   private static final long LARGE_ID_METRIC = 524288;
   private static final long LARGE_ID_TAG_KEY = 524290;
   private static final long LARGE_ID_TAG_VALUE = 524289;
+
+  @Test
+  public void testEqualsSameId() throws Exception {
+    final LabelId firstId = CassandraLabelId.fromLong(SMALL_ID);
+    final LabelId secondId = CassandraLabelId.fromLong(SMALL_ID);
+    assertTrue(firstId.equals(secondId));
+  }
+
+  @Test
+  public void testEqualsNotSameId() throws Exception {
+    final LabelId firstId = CassandraLabelId.fromLong(LARGE_ID_TAG_KEY);
+    final LabelId secondId = CassandraLabelId.fromLong(LARGE_ID_METRIC);
+    assertFalse(firstId.equals(secondId));
+  }
 
   @Test
   public void testGenerateIdHashes() {
